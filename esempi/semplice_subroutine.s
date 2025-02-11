182d41bdb7c5dc9f88b4272218bb5cdc5af3b36b
Massimo Cappellano <massimo.cappe@gmail.com>
2025-02-11 19:26:03 +0100

    Improv

---
+++ esempi/semplice_subroutine.s
@@ -0,0 +1,26 @@
+
+# il registro 1 contiene indirizzo della subroutine 
+li $1 100
+
+li $2 7
+
+# il registro 9 dopo esecuzione jalr il valore del PC di ritorno
+jalr $9 $1
+
+li $2 10
+jalr $9 $1
+
+li $2 3
+jalr $9 $1
+
+li $1 0         # trap number for Halt
+syscall         # halt
+
+# il registro 2 contiene il valore del lato
+# il registro 10 contiene il risultato dell'operazione (valore dell'area
+@100
+mul $10 $2 $2
+jalr $11 $9
+
+
+
