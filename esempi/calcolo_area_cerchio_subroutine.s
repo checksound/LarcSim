b9e890e4ee0183b26b6c4dfb3cbfd2a076fca98b
Massimo Cappellano <massimo.cappe@gmail.com>
2025-08-28 12:01:12 +0200

    improv

--- esempi/calcolo_area_cerchio_subroutine.s
+++ esempi/calcolo_area_cerchio_subroutine.s
@@ -1,31 +1,37 @@
 
-# il registro 1 contiene indirizzo della subroutine 
-li $1 100
+# costante 1000 (0x3E8 in esadecimale) nel registro 4
+lui $4 3
+li $5 0xE8 
+lui $6 0xFF
+sub $5 $5 $6
 
-li $2 7
+add $4 $4 $5  # caricata la costante nel registro 4
 
-# il registro 9 dopo esecuzione jalr il valore del PC di ritorno
-# il registro 1 contiene l'indirizzo della subroutine e viene caricato
-# nel PC (Program Counter) 
-jalr $9 $1
+# costante 3141 (0xC45 in esadecimale) nel registro 6
+lui $6 0xC
+li $7 0x45     # siccome < 0x80 non fa l'estensione di segno
 
-# controlla il valore registro 10 dovrebbe essere 49 (7 * 7)
+add $6 $6 $7   # caricata la costante nel registro 6
 
-li $2 10
-jalr $9 $1
+li $1 2    # chiamata di funzione con raggio = 2
+li $8 50
+jalr $9 $8
+# nel registro 2 controlla il risultato approssimato del calcolo area
 
-# controlla il valore registro 10 dovrebbe essere 100 (10 * 10)
-
-li $2 3
-jalr $9 $1
-
-# controlla il valore registro 10 dovrebbe essere 9 (3 * 3)
+li $1 5    # chiamata di funzione con raggio = 5
+li $8 50
+jalr $9 $8
+# nel registro 2 controlla il risultato approssimato del calcolo area
 
 li $1 0         # trap number for Halt
 syscall         # halt
 
-# il registro 2 contiene il valore del lato
-# il registro 10 contiene il risultato dell'operazione (valore dell'area)
-@100
-mul $10 $2 $2
+# calcola area cerchio
+# registro 1 contiene l'input, valore del lato
+# registro 2 contiene l'output - valore intero dell'area
+@50
+add $2 $0 $1
+mul $2 $2 $2    
+mul $2 $2 $6
+div $2 $2 $4
 jalr $0 $9
